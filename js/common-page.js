80903b0d4fa570dfd7a3dc883192be8e0b3baacc
wangkj <wangkj@yex360.com>
2019-02-22 18:30:36 +0800

    bug

--- js/common-page.js
+++ js/common-page.js
@@ -186,7 +186,7 @@ var commonPage = {
                     if(me.isloadNum > 0){
                         $.showLoading();//加载数据
                     }
-                }, 800);
+                }, 500);
                 self.$rightObj.find(self._options.rightContentContainerSelector).load(contentTplHtml, function(){
                     self[startTplFuncName].init(null, function(){
                         setTimeout(function(){
